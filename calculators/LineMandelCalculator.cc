31b7682e85593949ed41bc88d22e4397a8a89b7c
xbayer09@stud.fit.vutbr.cz <xbayer09@stud.fit.vutbr.cz>
2021-11-10 12:41:36 +0100

    sadf

--- calculators/LineMandelCalculator.cc
+++ calculators/LineMandelCalculator.cc
@@ -64,7 +64,7 @@ int * LineMandelCalculator::calculateMandelbrot()
 
 		for (int k = 0; k < limit; k++) {
 			
-#			pragma omp simd simdlen(64) linear(i, k)
+#			pragma omp simd simdlen(64) lastprivate(k)
 			for (int j = 0; j < width; j++) {
 
 				float x = x_start + j * dx; // current real value
