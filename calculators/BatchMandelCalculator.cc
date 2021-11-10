c6f079f7300f74ff8d8275cefa47fdf009d30467
xbayer09@stud.fit.vutbr.cz <xbayer09@stud.fit.vutbr.cz>
2021-11-10 17:10:27 +0100

    asdf

--- calculators/BatchMandelCalculator.cc
+++ calculators/BatchMandelCalculator.cc
@@ -16,6 +16,8 @@
 
 #include "BatchMandelCalculator.h"
 
+#define USE_INTRINSICS
+
 #define SIMD_512_ALIGNMENT 64
 #define BATCH_SIZE 128
 
@@ -56,7 +58,7 @@ BatchMandelCalculator::~BatchMandelCalculator()
 	zImag = nullptr;
 }
 
-
+#ifndef USE_INTRINSICS
 
 int * BatchMandelCalculator::calculateMandelbrot()
 {
@@ -96,3 +98,95 @@ int * BatchMandelCalculator::calculateMandelbrot()
 
 	return data;
 }
+
+#else
+
+#	define MM_PSIZE_32BIT 16
+
+static inline __attribute__((always_inline))
+__m512i mandelbrot(__m512 real, __m512 imag, int limit, __mmask16 mask)
+{
+	__m512i result = _mm512_setzero_epi32();
+	__mmask16 result_mask = mask;
+
+	const __m512 two = _mm512_set1_ps(2.f);
+	const __m512 four = _mm512_set1_ps(4.f);
+
+	__m512 zReal = real;
+	__m512 zImag = imag;
+
+	for (int i = 0; i < limit; i++) {
+	//	r2 = zReal * zReal
+		const __m512 r2 = _mm512_mul_ps(zReal, zReal);
+
+	//	i2 = zImag * zImag
+		const __m512 i2 = _mm512_mul_ps(zImag, zImag);
+
+	//	if (r2 + i2 > 4.0f) then write i to result and update result_mask
+		__mmask16 test_mask = _mm512_cmp_ps_mask(_mm512_add_ps(r2, i2), four, _CMP_GT_OS);
+
+		result = _mm512_mask_mov_epi32(result, test_mask & result_mask, _mm512_set1_epi32(i));
+		result_mask &= ~test_mask;
+
+		if (result_mask == 0x0000U)
+			return result;
+		
+	//	zImag = 2.0f * zReal * zImag + imag;
+		zImag = _mm512_fmadd_ps(_mm512_mul_ps(two, zReal), zImag, imag);
+
+	//	zReal = r2 - i2 + real;
+		zReal = _mm512_add_ps(_mm512_sub_ps(r2, i2), real);
+	}
+
+	result = _mm512_mask_mov_epi32(result, result_mask, _mm512_set1_epi32(limit));
+
+	return result;
+}
+
+int * BatchMandelCalculator::calculateMandelbrot()
+{
+	for (int i = 0; i < height; i += MM_PSIZE_32BIT) {
+		for (int j = 0; j < width; j += MM_PSIZE_32BIT) {
+			int h_limit = i + MM_PSIZE_32BIT;
+
+			if (h_limit >= height)
+				h_limit = height;
+
+			__mmask16 mask = 0xffffU;
+			int diff = width - j;
+			if (diff < MM_PSIZE_32BIT)
+				mask >>= MM_PSIZE_32BIT - diff;
+
+			int *pdata = data + i * width + i;
+
+			__m512 dx_ps, x_start_ps, inc_ps;
+
+			dx_ps = _mm512_set1_pd(static_cast<float>(dx));
+			x_start_ps = _mm512_set1_ps(static_cast<float>(x_start));
+
+			inc_ps = _mm512_set_ps(15.f, 14.f, 13.f, 12.f, 11.f, 10.f, 9.f, 8.f,
+						   		   7.f, 6.f, 5.f, 4.f, 3.f, 2.f, 1.f, 0.f);
+
+			for (int k = i; k < h_limit; k++) {
+				const __m512 y_ps = _mm512_set1_ps(y_start + k * dy);
+
+			//	prepare j
+				__m512 j_ps = _mm512_add_ps(_mm512_set1_ps(j), inc_ps);
+
+			//	x = x_start + j * dx
+				__m512 x_ps = _mm512_add_ps(x_start_ps, _mm512_mul_ps(j_ps, dx_ps));
+
+				__m512i values = mandelbrot(x_ps, y_ps, limit, mask);
+
+			//	store values in memory pointed by pdata using mask
+				_mm512_mask_storeu_epi32(pdata, mask, values);
+
+				pdata += width;
+			}
+		}
+	}
+
+	return data;
+}
+
+#endif	//	USE_INTRINSICS
